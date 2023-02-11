1c7abd3bd77ec1d3159e8440e32da2cf47ebd40f
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-02-11 18:06:48 +0900

    Initial Commit

---
+++ BetterReset/BetterResetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterResetApp.swift
+//  BetterReset
+//
+//  Created by 山本響 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct BetterResetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
